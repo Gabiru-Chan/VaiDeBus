9f2e8dde66321ddad4128b5d394971d1f7d913a3
Gabiru-Chan <gabriel.csilva.0520@gmail.com>
2023-08-15 17:20:33 -0400

    update project

--- src/services/firebaseConnection.js
+++ src/services/firebaseConnection.js
@@ -3,13 +3,13 @@ import 'firebase/database';
 import 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAwVWrpvU55-lc0PSVu2PJvG303m0kqQE8",
-  authDomain: "conectransv2.firebaseapp.com",
-  projectId: "conectransv2",
-  storageBucket: "conectransv2.appspot.com",
-  messagingSenderId: "1040878227678",
-  appId: "1:1040878227678:web:5d45b90ef86e8f8ff3b24f",
-  measurementId: "G-67ZY87EBMM"
+  apiKey: "AIzaSyARQPo7hcdINbaqxU6pL8IobxSYvFsnnos",
+  authDomain: "conecbasedata.firebaseapp.com",
+  projectId: "conecbasedata",
+  storageBucket: "conecbasedata.appspot.com",
+  messagingSenderId: "704248042446",
+  appId: "1:704248042446:web:46817fac4b0b3bc79b919f",
+  measurementId: "G-KGY720SDCJ"
 };
 
 if(!firebase.apps.length){
